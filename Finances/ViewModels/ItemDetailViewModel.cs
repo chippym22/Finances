96a65899bcef2555d42bdd1cd089802190265d35
Chris Morrison <chrismmorrison@hotmail.com>
2017-12-31 16:14:58 +0000

    Intial commit

---
+++ Finances/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Finances
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
---
+++ Finances/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Finances
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
